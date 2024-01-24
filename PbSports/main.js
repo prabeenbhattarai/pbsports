62d92e624d06babe09fa9f02a27fddee665996b9
prabeenbhattarai <52213722+prabeenbhattarai@users.noreply.github.com>
2024-01-24 20:15:30 +0545

    Update main.js

--- PbSports/main.js
+++ PbSports/main.js
@@ -2,6 +2,6 @@ $(window).on('load', function () {
     $('#m3u8-placeholder')[0].value = localStorage.getItem('m3u8-link') || '';
     $('#play-btn').on('click', function () {
         localStorage.setItem('m3u8-link', $('#m3u8-placeholder')[0].value);
-        window.location.href = './player' + '#' + $('#m3u8-placeholder')[0].value;
+        window.location.href = './main' + '#' + $('#m3u8-placeholder')[0].value;
     });
 });
